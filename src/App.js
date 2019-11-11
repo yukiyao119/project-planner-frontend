26d3ef010328649931a13315c9c9c6f3d0540c34
yukiyao119 <ukiyao@hotmail.com>
2019-11-11 12:35:19 -0500

    mvc without search sort and filter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import 'bulma/css/bulma.css';
 // import LogIn from './LogIn';
 import Nav from './Nav';
 import BodyContainer from './BodyContainer';
