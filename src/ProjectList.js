84f8598e06a66117655409321a3b86c55f1085e4
Brian Wong <bwong92@gmail.com>
2020-04-01 20:14:48 -0400

    projectlist

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react'
 
 
 export default class ProjectList extends Component {
-    addNewProject = () => {
-        console.log("CLICKED")
-    }
+    
     
     render() {
     console.log(this.props.projects)
@@ -12,8 +10,8 @@ export default class ProjectList extends Component {
             <div>
                 
                 <h2>Project List: </h2>
-                <button onClick={this.addNewProject}>Add New Project</button>
-                <br />
+                
+                
                 <h2>{this.props.projects.map( project => {
                     return <li onClick={() => this.props.handleShowCard(project)}>{project.name}</li>})}
                 </h2>
